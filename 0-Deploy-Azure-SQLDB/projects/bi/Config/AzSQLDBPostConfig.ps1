5f1e8399c59c280866ef1a092a5b8a6fefd5c2a8
David Barbarin <David.Barbarin@leshop.ch>
2020-06-04 19:20:58 +0200

    Add post scripts and automation

---
+++ 0-Deploy-Azure-SQLDB/projects/bi/Config/AzSQLDBPostConfig.ps1
@@ -0,0 +1,419 @@
+﻿Function Set-AzServerConfig{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]$SubId
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$ResourceGroupName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$ServerName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$omsId
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$ADSRecipientsEmails
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$ADSGroupResourceGroupName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$ADSStorageaccountName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$AlertActionGroupName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$AlertActionGroupResourceGroupName
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipAudit
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipTDE
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipAutomaticTuning
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipAlerts
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipADS
+    )
+
+    $WarningPreference = "SilentlyContinue"
+
+    Select-AzSubscription -SubscriptionName $SubId | Out-Null
+    $SubId = (Get-AzSubscription -SubscriptionName $SubId).SubscriptionId
+
+    Try {
+        If (-Not $SkipAudit.IsPresent){
+            # Configure Server Audit => Log analytics target not handled by Terraform
+            $AuditActionGroup = @('SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP', 'FAILED_DATABASE_AUTHENTICATION_GROUP', 'BATCH_COMPLETED_GROUP', 'DATABASE_PERMISSION_CHANGE_GROUP', 'DATABASE_PRINCIPAL_CHANGE_GROUP', 'DATABASE_ROLE_MEMBER_CHANGE_GROUP', 'USER_CHANGE_PASSWORD_GROUP')
+
+            Write-Host "[Azure SqlServer Config]" -NoNewline -ForegroundColor Yellow
+            Write-Host "Audit " -NoNewline
+
+            Set-AzSqlServerAudit `
+                -ResourceGroupName $ResourceGroupName `
+                -ServerName $serverName `
+                -AuditActionGroup $AuditActionGroup `
+                -LogAnalyticsTargetState Enabled `
+                -WorkspaceResourceId $omsId | Out-Null
+
+            Write-Host "configured successfully" -ForegroundColor Green
+        }
+
+
+        
+        # Firewall and virtual network handled by Terraform
+
+        If (-Not $SkipTDE.IsPresent){
+            # TDE method => Self Managed
+            Write-Host "[Azure SqlServer Config]" -NoNewline -ForegroundColor Yellow
+            Write-Host "TDE " -NoNewline
+
+            Set-AzSqlServerTransparentDataEncryptionProtector `
+                -ResourceGroupName $ResourceGroupName `
+                -ServerName $ServerName `
+                -Type ServiceManaged | Out-Null
+
+            Write-Host "configured successfully" -ForegroundColor Green
+        }
+
+        If (-Not $SkipAutomaticTuning.IsPresent){
+        # Automatic Tuning => Disabling ForceLastGoodPlan | DropIndex | CreateIndex
+            Write-Host "[Azure SqlServer Config]" -NoNewline -ForegroundColor Yellow
+            Write-Host "Automatic Tuning " -NoNewline
+
+            Set-AzSqlServerAdvisorAutoExecuteStatus `
+                -ResourceGroupName $ResourceGroupName `
+                -ServerName $SqlServer `
+                -AdvisorName ForceLastGoodPlan `
+                -AutoExecuteStatus Disabled | Out-Null
+
+            Set-AzSqlServerAdvisorAutoExecuteStatus `
+                -ResourceGroupName $ResourceGroupName `
+                -ServerName $SqlServer `
+                -AdvisorName DropIndex `
+                -AutoExecuteStatus Disabled | Out-Null
+
+            Set-AzSqlServerAdvisorAutoExecuteStatus `
+                -ResourceGroupName $ResourceGroupName `
+                -ServerName $SqlServer `
+                -AdvisorName CreateIndex `
+                -AutoExecuteStatus Disabled | Out-Null
+
+            Write-Host "configured successfully" -ForegroundColor Green
+        }
+
+        If (-Not $SkipADS.IsPresent){
+            # ADS
+            Write-Host "[Azure SqlServer Config]" -NoNewline -ForegroundColor Yellow
+            Write-Host "Advanced Data Security " -NoNewline
+
+            $StorageAccount = Get-AzStorageAccount -ResourceGroupName $ADSGroupResourceGroupName -Name $ADSStorageaccountName
+
+            If ($StorageAccount){
+                Enable-AzSqlServerAdvancedThreatProtection `
+                    -ResourceGroupName $ResourceGroupName `
+                    -ServerName $SqlServer | Out-Null
+
+                Update-AzSqlServerAdvancedThreatProtectionSetting `
+                    -ResourceGroupName $ResourceGroupName `
+                    -ServerName $SqlServer `
+                    -NotificationRecipientsEmails $ADSRecipientsEmails | Out-Null
+
+                Write-Host "configured successfully" -ForegroundColor Green
+            }
+            Else {
+                Write-Host "KO - ADS Storage Account not found" -ForegroundColor Red
+            }
+        }
+
+        If (-Not $SkipAlerts.IsPresent){
+            # Alerts
+            # Monitor Administrative operations (all databases)
+
+            $alert_name = "$SqlServer-DB-Administrative-ops"
+
+            Write-Host "[Azure SqlServer Config]" -NoNewline -ForegroundColor Yellow
+            Write-Host "Alert $alert_name " -NoNewline
+
+            $alert = Get-AzActivityLogAlert -ResourceGroupName $ResourceGroupName | Where-Object { $_.Name -eq $alert_name }
+            If ($alert) {
+                Write-Host "already exists" -ForegroundColor Green
+            }
+            Else{
+                $actiongroup1 = Get-AzActionGroup -ResourceGroup $AlertActionGroupResourceGroupName -Name $AlertActionGroupName
+
+                If ($actiongroup1){
+                    #$actiongroup2 = Get-AzActionGroup -ResourceGroup $ResourceGroupName -Name 'notif-DBA'
+                    $scope = "/subscriptions/$SubId/resourceGroups/$ResourceGroupName/providers/Microsoft.Sql/servers/$SqlServer/databases"
+                    $condition1 = New-AzActivityLogAlertCondition -Field 'category' -Equal 'Administrative'
+                    $condition2 = New-AzActivityLogAlertCondition -Field 'level' -Equal 'Informational'
+                    $condition3 = New-AzActivityLogAlertCondition -Field 'status' -Equal 'Succeeded'
+
+                    Set-AzActivityLogAlert `
+                        -Location 'Global' `
+                        -Name $alert_name  `
+                        -ResourceGroupName $ResourceGroupName `
+                        -Scope $scope `
+                        -Action (New-Object Microsoft.Azure.Management.Monitor.Models.ActivityLogAlertActionGroup $actiongroup1.Id) `
+                        -Condition $condition1, $condition2, $condition3 `
+                        -Description 'Get all SQL Administration events and check if misconfigured / unwanted items exist' | Out-Null
+
+                    Write-Host "created successfully" -ForegroundColor Green
+                }
+                Else {
+                    Write-Host "KO - Action Group not Found" -ForegroundColor Red
+                }
+            }
+        }
+
+    }
+    Catch{
+        Write-Host "KO" -ForegroundColor Red
+        Write-Host $PSItem.Exception.InnerException -ForegroundColor Red
+    }
+}
+
+Function Set-AzDatabaseConfig{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]$SubName
+        ,
+        [Parameter(Mandatory=$true)]
+        $ResourceGroupName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$ServerName
+        ,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+        [String[]]$DatabaseName
+        ,
+        [Parameter(Mandatory=$false)]
+        [String[]]$CriticalDBs
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$AlertActionGroupName
+        ,
+        [Parameter(Mandatory=$true)]
+        [String]$AlertActionGroupResourceGroupName
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipTDE
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipBackupPolicies
+        ,
+        [Parameter(Mandatory=$false)]
+        [Switch]$SkipAlerts
+    )
+
+    Begin {
+        $WarningPreference = "SilentlyContinue"
+
+        Select-AzSubscription -SubscriptionName $SubName | Out-Null
+        $SubId = (Get-AzSubscription -SubscriptionName $SubName).SubscriptionId
+
+        $pitr_retention_policy_Critical_DBs = 21
+        $pitr_retention_policy_NonCritical_DBs = 7
+        $tde_state_compliant = 'Enabled'
+    }
+    Process{
+        Foreach ($DB in $DatabaseName){
+            Try{
+                Write-Host "[Azure Database Config]" -NoNewline -ForegroundColor Yellow
+                Write-Host "==> Database $DB " -NoNewline
+
+                If (-Not (Get-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $ServerName | Where-Object { $_.DatabaseName -eq $DB })){
+                    Write-Host "doesn't exist ... skip next steps" -ForegroundColor Red
+                }
+                Else {
+                    Write-Host "exists ... go to next steps" -ForegroundColor Green
+                    If (-Not $SkipTDE.IsPresent){
+                        # Enabled TDE
+                        Write-Host "[Azure Database Config]" -NoNewline -ForegroundColor Yellow
+                        Write-Host "TDE " -NoNewline
+
+                        Set-AzSqlDatabaseTransparentDataEncryption `
+                            -ResourceGroupName $ResourceGroupName `
+                            -ServerName $ServerName `
+                            -DatabaseName $DB `
+                            -State $tde_state_compliant | Out-Null
+
+                        Write-Host "configured successfully" -ForegroundColor Green
+                    }
+
+                    If (-Not $SkipBackupPolicies.IsPresent){
+                        # Configure Backup PITR policy
+                        Write-Host "[Azure Database Config]" -NoNewline -ForegroundColor Yellow
+                        Write-Host "Backup PITR/LTR " -NoNewline
+
+                        If ($DatabaseName -in $CriticalDBs){
+                            Set-AzSqlDatabaseBackupShortTermRetentionPolicy `
+                                -ResourceGroupName $ResourceGroupName `
+                                -ServerName $ServerName `
+                                -DatabaseName $DB `
+                                -RetentionDays $pitr_retention_policy_Critical_DBs `
+                                -Confirm:$false | Out-Null
+                        }
+                        Else {
+                            Set-AzSqlDatabaseBackupShortTermRetentionPolicy `
+                                -ResourceGroupName $ResourceGroupName `
+                                -ServerName $ServerName `
+                                -DatabaseName $DB `
+                                -RetentionDays $pitr_retention_policy_NonCritical_DBs `
+                                -Confirm:$false | Out-Null
+                        }
+
+                        # Disable Backup LTR policy
+                        Set-AzSqlDatabaseBackupLongTermRetentionPolicy `
+                            -ResourceGroupName $ResourceGroupName `
+                            -ServerName $ServerName `
+                            -DatabaseName $DB `
+                            -RemovePolicy `
+                            -Confirm:$false | Out-Null
+
+                        Write-Host "configured successfully" -ForegroundColor Green
+                    }
+
+                    # ADS (Not enabled as the database level)
+                    # Configure Automatic Tuning (Inherit from Server)
+
+                    If (-Not $SkipAlerts.IsPresent){
+                        # Create alerts
+                        $actiongroup = Get-AzActionGroup -ResourceGroup $AlertActionGroupResourceGroupName -Name $AlertActionGroupName
+
+                        If ($actiongroup){
+                            $alert_name = "$ServerName-DB-$DB-Encryption"
+
+                            Write-Host "[Azure Database Config]" -NoNewline -ForegroundColor Yellow
+                            Write-Host "Alert: $alert_name " -NoNewline
+
+                            $alert = Get-AzActivityLogAlert -ResourceGroupName $ResourceGroupName | Where-Object { $_.Name -eq $alert_name }
+                            If ($alert) {
+                                Write-Host "already exists" -ForegroundColor Green
+                            }
+                            Else{
+                                # Monitor TDE changes
+                                $scope = "/subscriptions/$SubId/resourceGroups/$ResourceGroupName/providers/Microsoft.Sql/servers/$ServerName/databases/$DB/transparentDataEncryption/current"
+                                $condition1 = New-AzActivityLogAlertCondition -Field 'category' -Equal 'Administrative'
+                                $condition2 = New-AzActivityLogAlertCondition -Field 'level' -Equal 'Informational'
+                                $condition3 = New-AzActivityLogAlertCondition -Field 'status' -Equal 'Succeeded'
+
+                                Set-AzActivityLogAlert `
+                                    -Location 'Global' `
+                                    -Name $alert_name  `
+                                    -ResourceGroupName $ResourceGroupName `
+                                    -Scope $scope `
+                                    -Action (New-Object Microsoft.Azure.Management.Monitor.Models.ActivityLogAlertActionGroup $actiongroup.Id) `
+                                    -Condition $condition1, $condition2, $condition3 | Out-Null
+
+                                Write-Host "configured successfully" -ForegroundColor Green
+                            }
+
+                            # Monitor DB Audit Changes
+                            $alert_name = "$ServerName-DB-$DB-Audit"
+
+                            Write-Host "[Azure Database Config]" -NoNewline -ForegroundColor Yellow
+                            Write-Host "Alert: $alert_name " -NoNewline
+
+                            $alert = Get-AzActivityLogAlert -ResourceGroupName $ResourceGroupName | Where-Object { $_.Name -eq $alert_name }
+                            If ($alert) {
+                                Write-Host "already exists" -ForegroundColor Green
+                            }
+                            Else {
+                                $scope = "/subscriptions/$SubId/resourceGroups/$ResourceGroupName/providers/Microsoft.Sql/servers/$ServerName/databases/$DB/auditingSettings/default"
+                                $condition1 = New-AzActivityLogAlertCondition -Field 'category' -Equal 'Administrative'
+                                $condition2 = New-AzActivityLogAlertCondition -Field 'level' -Equal 'Informational'
+                                $condition3 = New-AzActivityLogAlertCondition -Field 'status' -Equal 'Succeeded'
+
+                                Set-AzActivityLogAlert `
+                                    -Location 'Global' `
+                                    -Name $alert_name  `
+                                    -ResourceGroupName $ResourceGroupName `
+                                    -Scope $scope `
+                                    -Action (New-Object Microsoft.Azure.Management.Monitor.Models.ActivityLogAlertActionGroup $actiongroup.Id) `
+                                    -Condition $condition1, $condition2, $condition3 | Out-Null
+
+                                Write-Host "configured successfully" -ForegroundColor Green
+                            }
+                        }
+                        Else {
+                            Write-Host "KO - Action Group not Found" -ForegroundColor Red
+                        }
+                    }
+                }
+            }
+            Catch{
+                Write-Host "KO" -ForegroundColor Red
+                Write-Host $PSItem.Exception.InnerException -ForegroundColor Red
+            }
+        }
+    }
+}
+
+################################################################## Azure Automation Schedule ##################################################################
+
+Login-AzAccount
+Clear
+
+# Global param
+$subscription = 'Visual Studio Ultimate avec MSDN'
+Select-AzSubscription -Subscription $subscription | Out-Null
+$SqlServer = 'azsqldbp-bi'
+$Database = 'Powersaturday'
+
+$subscriptionOMS = 'Visual Studio Ultimate avec MSDN'
+$ResourceGroupNameOMS = 'core-rg'
+$WS = 'azomsp-core'
+$subscriptionSQL = 'Visual Studio Ultimate avec MSDN'
+$ResourceGroupNameSQL = 'az-p-bi-database-rg'
+$SqlServerName = 'azsqldbp-bi'
+$ADSRecipientsEmails = 'mikedavem1@hotmail.com'
+$AlertActionGroupResourceGroupName = 'Default-ActivityLogAlerts'
+$AlertActionGroupName = 'Send_Email_To_Azure_Admins'
+$ADSGroupResourceGroupName = 'az-p-bi-database-rg'
+$ADSStorageaccountName = 'azstoreadspbi'
+
+Select-AzSubscription -Subscription $subscriptionOMS | Out-Null
+$oms = Get-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupNameOMS -Name $WS
+
+Select-AzSubscription -Subscription $subscriptionSQL | Out-Null
+
+Set-AzServerConfig `
+    -SubId $subscription `
+    -ResourceGroupName $ResourceGroupNameSQL `
+    -ServerName $SqlServerName `
+    -omsId $oms.ResourceId `
+    -ADSRecipientsEmails $ADSRecipientsEmails `
+    -AlertActionGroupName $AlertActionGroupName `
+    -AlertActionGroupResourceGroupName $AlertActionGroupResourceGroupName `
+    -ADSGroupResourceGroupName $ADSGroupResourceGroupName `
+    -ADSStorageaccountName $ADSStorageaccountName `
+    -SkipTDE -SkipAudit -SkipAutomaticTuning -SkipAlerts
+
+    
+################################################################## Azure SQL DBs Config ##################################################################
+$CriticalDBs = @()
+$DatabaseName = @('Powersaturday')
+
+$DatabaseName | Set-AzDatabaseConfig `
+    -SubName $subscriptionSQL `
+    -ResourceGroupName $ResourceGroupNameSQL `
+    -ServerName $SqlServerName `
+    -CriticalDBs $CriticalDBs `
+    -AlertActionGroupResourceGroupName $AlertActionGroupResourceGroupName `
+    -AlertActionGroupName $AlertActionGroupName 
+
+
+
+
+
+
+
---
+++ 1-Automation/azSQL-MaintenanceDB.ps1
@@ -0,0 +1,230 @@
+﻿param(
+        [parameter(Mandatory=$True)]
+        [string] $SqlInstance
+        ,
+        [parameter(Mandatory=$True)]
+        [string] $Database
+        ,
+        [parameter(Mandatory=$False)]
+        [int] $IdxOptRgsThreshold = 80
+        ,
+        [parameter(Mandatory=$False)]
+        [int] $IdxRebuildThreshold = 10
+        ,
+        [parameter(Mandatory=$False)]
+        [int] $StatisticsAgingThreshold = 7
+        ,
+        [parameter(Mandatory=$False)]
+        [String]$EnvTarget="ACC"
+        ,
+        [parameter(Mandatory=$False)]
+        [ValidateSet('idx_maintenance','stats_maintenance','idx_usage_stats','all')]
+        [String]$Action
+)
+
+# Choose the corresponding keyvault
+switch ($EnvTarget) {
+    "PROD" { $KeyvaultName = "azkv01p"}
+}
+
+Try {
+    # Run runbook as special account
+    $AzureAutomationConnectionName = "AzureRunAsConnection"
+    $ServicePrincipalConnection = Get-AutomationConnection -Name $AzureAutomationConnectionName
+
+    # Connect to get access to Key Vault
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -ApplicationId $ServicePrincipalConnection.ApplicationId `
+        -Tenant $ServicePrincipalConnection.TenantId `
+        -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint | Out-Null
+
+    # Get info for connection through PowerShell registred app which has access to the SQL Azure Dbs
+    $tenantName = $ServicePrincipalConnection.TenantId
+    $clientId = (Get-AzKeyVaultSecret -VaultName $KeyvaultName -Name "AZSQLSPwshAppId").SecretValueText
+    $clientSecret =  ConvertTo-SecureString (Get-AzKeyVaultSecret -VaultName $KeyvaultName -Name "AZSQLSPwshAppSecret").SecretValueText -AsPlainText -Force
+    $resourceUri = "https://database.windows.net/"
+    $authorityUri = "https://login.microsoftonline.com/$tenantName"
+    [String]$debugMsg = ''
+    $response = Get-ADALToken -ClientId $clientId -ClientSecret $clientSecret -Resource $resourceUri -Authority $authorityUri -TenantId $tenantName
+
+    # Connection String
+    $connectionString = "Server=tcp:$SqlInstance,1433;Initial Catalog=$Database;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;"
+
+    # Create the connection object
+    $connection = New-Object System.Data.SqlClient.SqlConnection($connectionString)
+
+    # Set AAD generated token to SQL connection token
+    $connection.AccessToken = $response.AccessToken
+
+    # Performs the requested action(s)
+    If ($action -eq 'idx_maintenance' -or $action -eq 'all'){
+    $debugMsg = 'idx_maintenance,'
+$query = @"
+EXEC [maintenance].[dbi_maintenance_user_indexes_databases]
+    @dbs_to_maintain = '$Database',
+    @debug = 0,
+    @hp_to_rebuild = -1,
+    @idx_to_rebuild = -1,
+    @p_idx_cci_opt_rgs_threshold = $IdxOptRgsThreshold,
+    @p_idx_cci_rebuild_threshold = $IdxRebuildThreshold;
+"@
+    }
+
+    If ($action -eq 'stats_maintenance' -or $action -eq 'all'){
+    $debugMsg += 'stats_maintenance,'
+$query += @"
+    `n`n
+EXECUTE [maintenance].[dbi_maintenance_user_statistics_databases]
+    @dbs_to_maintain = '$Database',
+    @debug = 0,
+    @p_statistics_aging_threshold = $StatisticsAgingThreshold,
+    @p_statistics_scan_method = 'FULLSCAN'
+"@
+    }
+
+    If ($action -eq 'idx_usage_stats' -or $action -eq 'all'){
+    $debugMsg += 'idx_usage_stats'
+$query += @"
+`n`n
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+DECLARE @max BIGINT = COALESCE((SELECT MAX(capture_id) FROM maintenance.ls_index_usage_stats), 0);
+
+DECLARE  @schema_name sysname = 'dbo'
+, @table_or_view_name sysname = NULL --= 'maTable' -- si NULL, Ã©tudie tous les index de toutes les table du schÃ©ma
+, @index_name sysname = NULL -- si NULL, Ã©tudie tous les index de la table
+, @separator varchar(2) = ', '
+----------------------------------------------------------------------------------------------------------------------
+INSERT INTO [maintenance].[ls_index_usage_stats]
+        ([capture_id]
+        ,[capture_date]
+        ,[schema_table_name]
+        ,[index_name]
+        ,[row_count]
+        ,[nb_partitions]
+        ,[type_desc]
+        ,[is_primary_key]
+        ,[is_unique]
+        ,[pages_count]
+        ,[index_size_MB]
+        ,[last_stat_update]
+        ,[user_seeks]
+        ,[user_scans]
+        ,[user_lookups]
+        ,[total_user_searches]
+        ,[last_user_seek]
+        ,[last_user_scan]
+        ,[last_user_lookup])
+SELECT DISTINCT
+    @max + 1 AS capture_id,
+    GETDATE() AS capture_date
+    , S.name + '.' + O.name AS schema_table_name
+    , I.name AS index_name
+    , PS.row_count
+    , PS.nb_partitions
+    , I.type_desc
+    , I.is_primary_key
+    , I.is_unique
+    , PS.used_page_count AS pages_count
+    , PS.used_page_count / 128 AS index_size_MB
+    , STATS_DATE(O.object_id, I.index_id) AS last_stat_update
+    , IUS.user_seeks
+    , IUS.user_scans
+    , IUS.user_lookups
+    , (IUS.user_seeks + IUS.user_scans + IUS.user_lookups) AS total_user_searches
+    , IUS.last_user_seek
+    , IUS.last_user_scan
+    , IUS.last_user_lookup
+FROM    sys.schemas AS S
+INNER JOIN  sys.objects AS O
+    ON S.schema_id = O.schema_id
+INNER JOIN  sys.indexes AS I
+    ON O.object_id = I.object_id
+INNER JOIN  sys.index_columns AS IC
+    ON IC.object_id = I.object_id
+    AND IC.index_id = I.index_id
+LEFT JOIN  sys.dm_db_index_usage_stats AS IUS
+    ON IUS.object_id = I.object_id
+    AND IUS.index_id = I.index_id
+    AND IUS.database_id = DB_ID()
+INNER JOIN  (
+    SELECT
+        PS1.object_id,
+        PS1.index_id,
+        COUNT(*) AS nb_partitions,
+        SUM(PS1.row_count) AS row_count,
+        SUM(PS1.used_page_count) AS used_page_count
+    FROM
+        sys.dm_db_partition_stats AS PS1
+    GROUP BY
+        PS1.object_id, PS1.index_id
+) AS PS
+ON PS.object_id = I.object_id
+AND PS.index_id = I.index_id
+WHERE    (@schema_name IS NULL OR S.name = @schema_name)
+AND    (@table_or_view_name IS NULL OR O.name = @table_or_view_name)
+AND    (@index_name IS NULL OR I.name = @index_name)
+ORDER BY  schema_table_name
+"@
+    }
+
+    # Opens connection to Azure SQL Database and executes a query
+    # With Serverless if DB is in auto-pause we need to retry the connection a second time
+    $logonAttempt = 0
+    $connectionResult = $False
+
+    Write-Output "$(Get-Date): Trying to $SqlInstance instance and $Database database ..."
+
+    while(!($connectionResult) -And ($logonAttempt -le 3)) {
+        $LogonAttempt++
+
+        Try {
+            # First attempt
+            $connection.Open()
+            $connectionResult = $True
+        }
+        Catch {
+            $connectionResult = $False
+            Start-Sleep -Seconds 20
+        }
+    }
+
+    If ($logonAttempt -gt 3){
+        Throw "$(Get-Date): Error connecting to the $SqlInstance instance and $Database database after more $logonAttempt attempts ..."
+    }
+    Else {
+        # After this, the token is no longer there, I believe this is because the authentication went through already, so it gets rid of it.
+        #$connection
+        Write-Output "$(Get-Date): Executing $debugMsg task(s) against $SqlInstance instance and $Database database ..."
+
+        $command = New-Object -Type System.Data.SqlClient.SqlCommand($query, $connection)
+        $command.CommandTimeout = 0
+        $command.ExecuteNonQuery()
+
+        $connection.Close()
+
+        Write-Output "$(Get-Date): Maintenance tasks $debugMsg have been performed on $SqlInstance instance and $Database database ..."
+
+        [String]$Subject = "AzSql-MaintenanceDB - Env: $EnvTarget - Server: $SqlInstance - DB: $Database OK"
+        [String]$Body = "AzSql-MaintenanceDB completed sucessfully for the task(s): $debugMsg"
+
+        Invoke-LSSendEmail `
+            -SmtpServer $smtpserver `
+            -Port 25 `
+            -UseSSL $True `
+            -From 'azure.automation@leshop.ch' `
+            -To 'DL_SQLAdmins@leshop.ch','DL_ECU_IT_DATA@leshop.ch' `
+            -Subject $Subject `
+            -Body $Body
+    }
+}
+Catch {
+    [String]$Subject = "AzSql-MaintenanceDB - Env: $EnvTarget - Server: $SqlInstance - Task(s): $tasks - DB: $Database KO"
+    [String]$Body = $_.Exception.Message
+
+    Write-Output $Subject
+    Write-Output $Body
+}
+
